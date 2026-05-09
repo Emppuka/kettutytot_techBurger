ea205d94d8882806d740128e6cd2f904b433a78d
Emmi Kallio <164744338+Emppuka@users.noreply.github.com>
2026-05-09 11:04:50 +0300

    moved productlist to homeview

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
-import ProductList from './components/ProductList';
 import Footer from './components/Footer';
+import HomeView from './views/HomeView';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
     <div className="bg-gray-50 min-h-screen flex flex-col">
     <Header></Header>
       <h1 className="text-center bg-red-400 text-4xl py-4">Template for the TechBurger project</h1>
-      <main className="flex-grow">
+      <main className="grow">
         <Routes>
-          <Route path="/" element={<ProductList />} />
+          <Route path="/" element={<HomeView />} />
         </Routes>
       </main>
       <Footer/>
---
+++ src/views/HomeView.tsx
@@ -0,0 +1,11 @@
+import ProductList from '../components/ProductList';
+
+const HomeView = () => {
+  return (
+    <main className="grow">
+      <ProductList />
+    </main>
+  );
+};
+
+export default HomeView;
