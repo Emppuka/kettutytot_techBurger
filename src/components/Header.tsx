5d1a2ac10cdcee9e0527693646c867fb13b21687
Juho <e2402268@edu.vamk.fi>
2026-05-16 10:28:38 +0300

    bugfixes, topbanner/ footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function Header(){
     const cartItems = useCartStore((state)=> state.cartItems);
 return(
-    <header>
-        <h1 className="bg-orange-500 text-white font-bold flex justify-between items-center sticky top-0 z-50">
+    <header className="fixed top-0 left-0 w-full z-50">
+        <h1 className="bg-orange-500 text-white font-bold flex justify-between items-center">
             <Link to='/'>
             <span className="ml-3">TechBurger</span>
             </Link>
             <Link to='/cart'>
-            <span className= "bg-orange-600 mr-3">{cartItems.length} tuotetta</span>
+            <span className= "mr-3">{cartItems.length} tuotetta</span>
             </Link>
         </h1>
     </header>
