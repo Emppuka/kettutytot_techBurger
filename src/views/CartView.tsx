c838eaf5fe8d14e0340ccf46101a499a5bb51b37
Juho <e2402268@edu.vamk.fi>
2026-05-16 11:22:19 +0300

    Added checkout and clearing the cart

--- src/views/CartView.tsx
+++ src/views/CartView.tsx
@@ -1,9 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useCartStore } from '../store/useCartStore';
 
 const CartView = () => {
+    const navigate = useNavigate();
     const {cartItems} = useCartStore();
     const totalPrice = cartItems.reduce((acc , item) => acc + item.price, 0);
+    const clearCart = useCartStore((state) => state.clearCart);
+
+    const handleCheckout = () => {
+        clearCart();           // 1. Tyhjennetään ostoskori taustalta
+        navigate("/checkout");
+    }
 
     return(
         <main className="p-4">
@@ -16,15 +23,15 @@ const CartView = () => {
                     <h2 className='text-xl font-semibold'>You cart is empty!</h2>
                 </div>
             ): (
-                <div className=''>{cartItems.map((item, index) => (
+                <div className=''>{cartItems.map((item) => (
                     <div key={item.id} className='flex items-center gap-4 p-4 border-b border-gray-100'>
-                        <div className='flex items-center gap-4 p-4 border-b border-gray-100'>
+                        
                             <img src={item.image}
                              alt={item.name}
                              className='w-16 h-16 object-cover rounded-lg' />
                             <h3 className="font-semibold text-lg">{item.name}</h3>
                             <h3 className="font-semibold text-lg">{item.price}€</h3>
-                        </div>
+                        
                     </div>
                 ))}
                 <div className="mt-8 p-6 bg-gray-50 rounded-2xl">
@@ -33,7 +40,9 @@ const CartView = () => {
               <span className="text-green-600">{totalPrice.toFixed(2)} €</span>
             </div>
             
-            <button className="w-full bg-orange-500 text-white py-4 rounded-xl font-bold">
+            <button 
+            onClick={handleCheckout}
+            className="w-full bg-orange-500 text-white py-4 rounded-xl font-bold">
               Proceed to Checkout
             </button>
           </div>
